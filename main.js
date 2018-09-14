5aa63c60140fc658b6f715264acc0ed0c250c7be
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-09-14 01:37:26 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const {app, BrowserWindow, globalShortcut} = require('electron');
+function createWindow () {
+    win = new BrowserWindow({
+        icon: 'logo.png',
+        width: 800,
+        height: 500,
+        center: true,
+        resizable: false,
+    })
+    win.setMenu(null);
+    win.loadFile('index.html');
+
+    globalShortcut.register('CommandOrControl+Shift+I', () => {
+        win.webContents.openDevTools()
+    });
+
+    globalShortcut.register('F5', () => {
+        win.reload();
+    });
+}
+
+app.on('ready', createWindow);
